664cfbafbe16e091af10946f7e4c05f7d74468a1
ShresthaSwapnil <shresthaswapnil03@gmail.com>
2024-11-28 21:06:59 +0545

    Upto About Section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      Disc<b>o</b>ver the largest event hosted by MVIC Students
+    </div>
+  );
+};
+
+export default AnimatedTitle;
